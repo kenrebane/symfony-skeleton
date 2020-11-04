c22c8b3dc35d10f0106b1335ef3e322dbcbc885e
Ken Rebane <ken.rebane@tv3.ee>
2020-11-04 03:08:56 +0200

    wtf

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -10,45 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
-    private $client;
-
-    public function __construct(VwsClient $client)
-    {
-        $this->client = $client;
-    }
 
     /**
      * @Route("/", name="test")
      */
     public function index(): Response
     {
-        /*
-         * country EE
-         * ref OTT_SEFLSERVICE
-         * custnum 258906
-         * finaccnum 258906
-         * currency EUR
-         * amount 61.00
-         * desc desc
-         * bank hanzaee
-         * service_id Paysera
-         * internalId null
-         * status new
-         * */
-        $payment = new PayseraPayment();
-        $payment->setCountry('EE');
-        $payment->setCustnum('258906');
-        $payment->setFinaccNum('258906');
-        $payment->setNormalAmount('61.00');
-        $payment->setBank('hanzaee');
-
-        dump($payment);
-        $this->client->setPayment($payment);
-
-        if ($this->client->savePayment())
-        {
-
-        }
 
         return $this->json([
             'message' => 'Welcome to your new controller!',
